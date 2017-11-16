4a94b38c40fafea81cd162ddbd24e44ff7573f79
zulaika <zulaika@etxea.local>
2017-11-16 21:57:42 +0100

    Updated UserVO.class to match DB

--- src/main/java/org/athenea/dao/UserVO.java
+++ src/main/java/org/athenea/dao/UserVO.java
@@ -8,6 +8,8 @@ public class UserVO {
   private String email;   // primary key
   private String password;
   private String name;
+  private String username;
+  private String phone_number;
 
   public UserVO(String email, String password, String name) {
     this.email = email;
@@ -37,5 +39,21 @@ public class UserVO {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPhone_number() {
+    return phone_number;
+  }
+
+  public void setPhone_number(String phone_number) {
+    this.phone_number = phone_number;
   }
 }
